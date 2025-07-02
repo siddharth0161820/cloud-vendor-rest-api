349e961871c7f9189743deaaab2019e035c385a3
siddharth0161820 <siddharth0161820@gmail.com>
2025-07-02 15:07:42 +0530

    🔥 Deleted old RESTAPI folder for clean deployment

--- RESTAPI/src/main/java/com/SPRINGBOOTRESTAPI/RESTAPI/RepositoryLayer/RepositoryLayer.java
+++
@@ -1,9 +0,0 @@
-package com.SPRINGBOOTRESTAPI.RESTAPI.RepositoryLayer;
-
-import com.SPRINGBOOTRESTAPI.RESTAPI.Entity.CloudVendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryLayer extends JpaRepository<CloudVendor,Integer> {
-}
